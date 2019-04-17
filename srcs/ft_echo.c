4bf1e084bc43f2b3e2f7296f905bc700b2f1013c
Abdelkarim Moutik <amoutik@e2r10p14.1337.ma>
2019-04-17 10:45:30 +0100

    Added -n option to echo

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: zoulhafi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/02 11:48:01 by zoulhafi          #+#    #+#             */
-/*   Updated: 2019/04/05 19:51:14 by amoutik          ###   ########.fr       */
+/*   Updated: 2019/04/17 10:22:15 by amoutik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,18 @@
 
 void		ft_echo(char **args, t_list **env)
 {
+	/*
+	** This flag is used to indecated whether we should print the new line or not
+	*/
+	int flag;
+
+	flag = 0;
 	(void)env;
+	if ((*args)[0] == '-' && (*args)[1] == 'n')
+	{
+		flag = 1;
+		args++;
+	}
 	while (*args)
 	{
 		ft_printf("%s", *args);
@@ -26,5 +37,6 @@ void		ft_echo(char **args, t_list **env)
 			ft_putchar(' ');
 		args++;
 	}
-	ft_printf("\n");
+	if (!flag)
+		ft_printf("\n");
 }
