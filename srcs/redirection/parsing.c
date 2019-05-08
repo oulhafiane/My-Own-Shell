3285b59bce921b7d96f32e011312e1a1e57e55fd
Zakariaa Oulhafiane <zoulhafi@e2r11p1.1337.ma>
2019-05-08 16:09:46 +0000

    Now  can be printed inside heredoc

--- srcs/redirection/parsing.c
+++ srcs/redirection/parsing.c
@@ -6,28 +6,58 @@
 /*   By: zoulhafi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 23:21:38 by zoulhafi          #+#    #+#             */
-/*   Updated: 2019/05/08 01:09:52 by zoulhafi         ###   ########.fr       */
+/*   Updated: 2019/05/08 16:07:47 by zoulhafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
+static void	parse_string(char **doc, char *line, char flag_dollar)
+{
+	char		*str;
+	t_string	str_dollar;
+
+	if (flag_dollar && ft_strchr(line, '$'))
+	{
+		str_dollar.string = NULL;
+		new_string(&str_dollar);
+		while (*line)
+		{
+			if (*line == '$')
+				handle_dollar(&line, &str_dollar);
+			push(&str_dollar, *line);
+			line++;
+		}
+		str = ft_strdup(str_dollar.string);
+		free_string(&str_dollar);
+	}
+	else
+		str = ft_strdup(line);
+	if (*doc != NULL)
+		*doc = ft_strjoin_pre_free(*doc, "\n", str);
+	else
+		*doc = ft_strdup(str);
+	ft_strdel(&str);
+}
+
 static char	*getdoc(t_token *token)
 {
 	t_line	*line;
 	char	*doc;
+	char	flag_dollar;
 
 	free_line();
 	line = init_line();
 	line->print_msg = 0;
 	doc = NULL;
+	flag_dollar = 0;
+	if (!(token->tok_type & SH_QUOTED) && !(ft_strchr(token->token, '\'') 
+			|| ft_strchr(token->token, '"')))
+		flag_dollar = 1;
 	while (read_line(line) == 0 && ft_strcmp(line->command, token->token) != 0
 			&& line->command[line->index] != EOT_KEY)
 	{
-		if (doc != NULL)
-			doc = ft_strjoin_pre_free(doc, "\n", line->command);
-		else
-			doc = ft_strdup(line->command);
+		parse_string(&doc, line->command, flag_dollar);
 		free_line();
 		init_line();
 		line->print_msg = 0;
